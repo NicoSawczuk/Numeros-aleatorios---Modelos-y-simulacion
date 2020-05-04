7460d12caccda76b1b26eec040d79c71276e9d81
Javier <kachuka99@gmail.com>
2020-05-04 19:28:40 -0300

    Avance en metodo congruencia mixto

---
+++ metodo/congruenciaMixto.js
@@ -0,0 +1,26 @@
+function metodoCongruenciaMixto(semilla , a, c, m , iteraciones ){
+
+    console.log(semilla)
+    console.log(a)
+    console.log(c)
+    console.log(m)
+    console.log(iteraciones)
+
+    var x = 0 ;
+    var array = [['Iteracion 0: ', semilla]] ;
+    array.push()
+    //34982
+    //52115
+    for (let index = 0; index < iteraciones; index++) {
+
+        console.log('a por semilla' + a *semilla) ;
+        console.log('res mod m' + ((a * semilla) + c)%m ) ;
+        x = ((a * semilla) + c)%m ;
+        array.push(['Iteracion ' + (index + 1), x]) ;
+        semilla = x ;
+        
+    }
+
+    return array ;
+
+}
