02bf0ec77c73f1e4117cd84cc77fc5803125506e
Nicolas <nico.290698@gmail.com>
2020-05-04 16:29:49 -0300

    Metodo de Von Neuman
    Se termino el metodo de Von Neuman, incluyendo sus vistas

---
+++ metodo/vonNeuman.js
@@ -0,0 +1,42 @@
+function metodoVonNeuman(semilla, n) {
+    var x; //Este es el numero semilla que se va a transformar en los N numeros pseudoaleatorios
+    var arregloSucesiones = [];
+    //Guardamos la primer semilla
+    arregloSucesiones.push(['Sucesión 0', semilla]);
+    for (let i = 0; i < n; i++) {
+
+        x = semilla * semilla;
+        //Transformamos x a un string para poder obtener los cuatro numeros centrales
+        semillaX = String(x);
+        //Comprobamos que la potencia tenga 8 digitos, de lo contrario, completamos dicho numero con ceros a la izquiera
+
+        if (semillaX.length < 8) {
+            var semillaXAux = "";
+            for (let index2 = 0; index2 < (8 - semillaX.length); index2++) {
+                semillaXAux += "0";
+            }
+            semillaX = semillaXAux + semillaX;
+
+        }
+
+        //Declaramos un auxiliar para x para poder tomar los 4 numeros centrales
+        var xAux = "";
+        for (let index = 2; index < semillaX.length - 2; index++) {
+            xAux += semillaX[index];
+        }
+
+        //Ahora comprobamos que xAux (nuestro numero siguiente de la sucecion) no contenga dos ceros al final
+        if (xAux[xAux.length] == "0" && xAux[xAux.length - 1] == "0") {
+            xAux[xAux.length - 1] == "1"
+            xAux[xAux.length] == "3";
+        }
+
+        arregloSucesiones.push(['Sucesión ' + (i + 1), xAux]);
+
+        //Ahora la variable semilla va a tomar el valor de xAux
+        semilla = parseInt(xAux);
+    }
+
+    return arregloSucesiones;
+
+};
