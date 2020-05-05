01fe451985ad016e9a0812a688048e9c1e5dc2fd
Nicolas <nico.290698@gmail.com>
2020-05-05 12:18:20 -0300

    Corrección del metodo de Congruencias Mixto
    Se castearon los argumentos recibidos a enteros, solucionando el problema anterior.

--- metodo/congruenciaMixto.js
+++ metodo/congruenciaMixto.js
@@ -1,4 +1,4 @@
-function metodoCongruenciaMixto(semilla , a, c, m , iteraciones ){
+function metodoCongruenciaMixto(semilla, a, c, m, iteraciones) {
 
     console.log(semilla)
     console.log(a)
@@ -6,21 +6,29 @@ function metodoCongruenciaMixto(semilla , a, c, m , iteraciones ){
     console.log(m)
     console.log(iteraciones)
 
-    var x = 0 ;
-    var array = [['Iteracion 0: ', semilla]] ;
+    semilla = parseInt(semilla);
+    a = parseInt(a);
+    c = parseInt(c);
+    m = parseInt(m);
+    iteraciones = parseInt(iteraciones);
+
+    var x = 0;
+    var array = [
+        ['Iteracion 0', semilla]
+    ];
     array.push()
-    //34982
-    //52115
+        //34982
+        //52115
     for (let index = 0; index < iteraciones; index++) {
 
-        console.log('a por semilla' + a *semilla) ;
-        console.log('res mod m' + ((a * semilla) + c)%m ) ;
-        x = ((a * semilla) + c)%m ;
-        array.push(['Iteracion ' + (index + 1), x]) ;
-        semilla = x ;
-        
+        console.log('a por semilla' + a * semilla);
+        console.log('res mod m' + ((a * semilla) + c) % m);
+        x = ((a * semilla) + c) % m;
+        array.push(['Iteracion ' + (index + 1), x]);
+        semilla = x;
+
     }
 
-    return array ;
+    return array;
 
 }
