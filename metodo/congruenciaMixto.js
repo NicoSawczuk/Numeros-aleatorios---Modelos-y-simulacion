6eb4fecbbf18df56c29489d7bde566e1a8930b4c
Javier <kachuka99@gmail.com>
2020-05-05 13:38:04 -0300

    fin metodo congruencia mixto

--- metodo/congruenciaMixto.js
+++ metodo/congruenciaMixto.js
@@ -1,11 +1,5 @@
 function metodoCongruenciaMixto(semilla, a, c, m, iteraciones) {
 
-    console.log(semilla)
-    console.log(a)
-    console.log(c)
-    console.log(m)
-    console.log(iteraciones)
-
     semilla = parseInt(semilla);
     a = parseInt(a);
     c = parseInt(c);
@@ -17,12 +11,8 @@ function metodoCongruenciaMixto(semilla, a, c, m, iteraciones) {
         ['Iteracion 0', semilla]
     ];
     array.push()
-        //34982
-        //52115
     for (let index = 0; index < iteraciones; index++) {
 
-        console.log('a por semilla' + a * semilla);
-        console.log('res mod m' + ((a * semilla) + c) % m);
         x = ((a * semilla) + c) % m;
         array.push(['Iteracion ' + (index + 1), x]);
         semilla = x;
