7786de5fd2f818841a628f5aa106857a9ca55cb0
Javier <kachuka99@gmail.com>
2020-05-18 11:15:21 -0300

    cambios

--- metodo/marcaClase.js
+++ metodo/marcaClase.js
@@ -2,7 +2,8 @@ function calcularMarcaClase(arrayProbabilidades){
 
     
     for (let i = 0; i < arrayProbabilidades.length; i++) {
-        const element = array[i];
+        
+        
         
     }
 
